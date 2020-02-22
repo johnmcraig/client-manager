62b2a4c2b3a551c84730d7c690f8f064a144910f
johnmcraig <jmcraig32884@gmail.com>
2020-02-21 21:59:33 -0500

    Arranged nav-items into readable format

--- src/ClientManager/client-vue/src/components/ui/Navbar.vue
+++ src/ClientManager/client-vue/src/components/ui/Navbar.vue
@@ -4,8 +4,15 @@
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav class="flex-grow-1 justify-content-center">
-        <b-nav-item class="nav-link text-dark btn btn-outline-light mt-2 ml-2" :to="{ name: 'EmployeeForm' }" >Employee Form</b-nav-item>
-        <b-nav-item class="nav-link text-dark btn btn-outline-light mt-2 ml-2" :to="{ name: 'EmployeeTable' }" >Employee Table</b-nav-item>
+        <b-nav-item
+          class="nav-link text-light btn btn-outline-light ml-2 mb-1"
+          :to="{ name: 'EmployeeForm' }"
+        >Employee Form</b-nav-item>
+
+        <b-nav-item
+          class="nav-link text-light btn btn-outline-light ml-2 mb-1"
+          :to="{ name: 'EmployeeTable' }"
+        >Employee Table</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
@@ -22,6 +29,6 @@ nav {
   background-color: #3274d3;
 }
 .nav a {
-    color: #fff;
+  color: #fff;
 }
 </style>
