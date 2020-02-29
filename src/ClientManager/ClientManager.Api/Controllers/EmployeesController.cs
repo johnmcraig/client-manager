7b3caab3a6af854db6eb166ec3670eafd10c151b
johnmcraig <jmcraig32884@gmail.com>
2020-02-29 16:10:55 -0500

    Cleaned up unused namespaces

--- src/ClientManager/ClientManager.Api/Controllers/EmployeesController.cs
+++ src/ClientManager/ClientManager.Api/Controllers/EmployeesController.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using ClientManager.Application.Employees;
 using ClientManager.Domain;
 using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClientManager.Controllers
